0b3af82df0710bfadd2fe95bfae4fa1a245ac5ed
HarshithManjunath <harshithmanjunath77@gmail.com>
2021-08-04 03:57:06 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from typing import Optional
 import os
+# import getpass
+import stdiomask
 
 print("*" *80)
 print("\t\t\tCHEMIST BILLING SYSTEM")
@@ -30,21 +32,20 @@ def login(username,password):
             print("Logged in successfully")
             main()
             break
-        else:
-            print("Wrong username or password")
-            exit(0)
-            print(" ")
+    print("Wrong username or password")
+    exit(0)
+    print(" ")
 
 def access(option):
     global name
     if(option == "l"):
         username = input("Enter your username: ")
-        password = input("Enter your password: ")
+        password = stdiomask.getpass()
         login(username,password)
     else:
         print("Enter your username and password to register")
         username = input("Username: ")
-        password = input("Password: ")
+        password = stdiomask.getpass()
         register(username,password)
 
 def main():
@@ -70,7 +71,7 @@ def main():
         delete()
         return
     if (num == 3):
-        # update()
+        update()
         return
     if (num == 4):
         search()
@@ -83,7 +84,7 @@ def main():
         return
     if (num == 7):
         j = 1
-        # bill(j)
+        bill()
         return
     if (num == 8):
         exit(0)
@@ -137,24 +138,59 @@ def delete():
     else:
         main()
 
-# def update():
-#     db = open("MEDICINEDB.txt","r")
-#     med = str(input("Enter medicine name: "))
-#     flag = True
-#     index = 0
-#     for line in db:
-#         index += 1
-#         l = line.split("|")
-#         if med == l[0]:
-#             flag == False
-#             break
-#     if flag:
-#         print(med, " found in records")
-#         while True:
-#             t = open("MEDICINEDB.txt")
-#             lines = t.readlines()
-#             str1 = lines[index -1]
-#             lines.remove(str1)
+def update():
+    string1 = input("Enter medicine to update:")
+    file1 = open("MEDICINEDB.txt", "r")
+    flag = 0
+    index = 0
+    for line in file1:
+        index += 1
+        if string1 in line:
+            lt = line.split(" ")
+            if string1 == lt[0]:
+                flag = 1
+                break
+    if flag == 0:
+        print('Medicine', string1, 'Not Found')
+    else:
+        print('Medicine', string1, 'Found In Line', index)
+        with open("MEDICINEDB.txt.txt") as f:
+            lines = f.readlines()
+            str1 = lines[index - 1]
+            lines.remove(str1)
+            lst1 = str1.split(" ")
+            print("Select Action to Modify the Medicine Details: ")
+            print("1. Modify PPU")
+            print("2. Modify Quantity")
+            print("3. Modify Date of manufacture")
+            print("4. Modify Date of expiry")
+            ch1 = int(input("Enter your choice: "))
+            if ch1 == 1:
+                pr = input("Enter the new price: ")
+                lst1[2] = pr
+            if ch1 == 2:
+                qt = input("Enter the new quantity: ")
+                lst1[3] = qt
+            if ch1 == 3:
+                mfg = input("Enter the new manufacture date: ")
+                lst1[4] = mfg
+            if ch1 == 4:
+                exp = input("Enter the new expiry date: ")
+                lst1[5] = exp
+            strg = " ".join(lst1)
+            lines.append(strg)
+            udate_medicine = open("MEDICINEDB.txt", "w")
+            udate_medicine.truncate(0)
+            for item in lines:
+                udate_medicine.write(item)
+            udate_medicine.close()
+    file1.close()
+    test = int(input("Press 1 to continue search medicine or Press 0 to exit: "))
+    if test == 1:
+        update()
+    else:
+        main()
+            
 
 def search():
     string1 = input("Enter medicine to search: ")
@@ -223,14 +259,53 @@ def view():
     main()
 
 def bill():
-    while True:
-        print("*"*80)
-        print("\t\t\t\tBILL")
-        print("*"*80)
-        cust = input("Enter customer name: ")
-        bdate = input("Enter billing date: ")
-        i = 0
-        
+    print("*"*80)
+    print("\t\t\t\tBILL")
+    print("*"*80)
+    cust = input("Enter customer name: ")
+    bdate = input("Enter billing date: ")
+    i = 0
+    index = 0
+    flag = 1
+    fbill = 0
+    items = int(input("Enter number of items: "))
+    for i in range(items):
+        f = open("MEDICINEDB.txt")
+        lines = f.readlines()
+        med = input("Enter the medicine: ")
+        for line in lines:
+            index += 1
+            lst = line.split("|")
+            if(lst[0] != med):
+                continue
+            print("Medicine in stock")
+            nos = int(input("Enter the nos: "))
+            cost = int(lst[2])*nos
+            fbill = fbill + cost
+            
+            with open("MEDICINEDB.txt") as f:
+                lines = f.readlines()
+                str1 = lines[index - 1]
+                lines.remove(str1)
+                lst = str1.split("|")
+                quan = float(lst[3]) - float(nos)
+                lst[3] = str(quan)
+                strg = "|".join(lst)
+                lines.append(strg)
+                udate_medicine = open("MEDICINEDB.txt", "w")
+                udate_medicine.truncate(0)
+                for med in lines:
+                    # udate_medicine.write("\n")
+                    udate_medicine.write(med)
+                udate_medicine.close()
+
+            flag = 0
+    if (flag == 0):
+        print("Total cost of the medicines has come up to: " + str(fbill))
+    else:
+        print("Medicine not found")
+    main()
+
 
 begin()
 access(option)
